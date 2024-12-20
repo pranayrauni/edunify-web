c9e4a9f82af46ce11a9a22a755bbe46aa61b5172
pranayrauni <ipranaysingh@gmail.com>
2024-12-20 22:01:46 +0530

    railway database url

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,13 @@
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  port: process.env.DATABASE_PORT,
+  // host: process.env.DATABASE_HOST,
+  // user: process.env.DATABASE_USER,
+  // password: process.env.DATABASE_PASSWORD,
+  // database: process.env.DATABASE_NAME,
+  // port: process.env.DATABASE_PORT,
+
+  uri: process.env.DATABASE_URL,
 });
 
 module.exports = db;
